eb49ad2d0eb927943d660260c09112246cc53303
pzukin <pzukin@gmail.com>
2013-07-22 15:08:59 -0700

    Constructed new rotation independent features

--- digitRot.py
+++ digitRot.py
@@ -1,7 +1,7 @@
 '''
 This script explores how well we can distinguish digits
 based on the ratio of a digits moment of inertia
-tensor.
+eigenvalues.
 
 '''
 
@@ -11,13 +11,13 @@ import matplotlib.font_manager
 import matplotlib.cm as cm
 import csv
 import utils
+from sklearn import svm
+from sklearn import cross_validation
 
-def plotHistEig(dat, tar):
+def plotHistRot(datRot, tar):
 
     '''
-    This function calculates moment of inertia tensor for each record.
-    Then takes the ratio of the eigenvalues and plots a histogram 
-    for each digit.
+    Plots distributions of the rotation independent features
 
     Parameters:
     -----------
@@ -27,58 +27,162 @@ def plotHistEig(dat, tar):
     '''
 
     # generate figure
+    matplotlib.rc('xtick.major', size=7.5)
+    matplotlib.rc('xtick.minor', size=7.5)
+    matplotlib.rc('ytick.major', size=7.5)
+    matplotlib.rc('ytick.minor', size=7.5)
     fig = plt.figure(1, figsize = (10, 10))
-    ax = fig.add_subplot(1, 1, 1)
     lsv=['-','-','-','-','-','-','-','--','--','--']
-    prop = matplotlib.font_manager.FontProperties(size=10.5)
-
-    # mapping of index in record to x,y coordinates
-    xAll = np.array( [i % 28 for i in xrange(0, 28**2)] )
-    yAll = np.array( [28 - i / 28 for i in xrange(0, 28**2)] )
+    prop = matplotlib.font_manager.FontProperties(size = 9.5)
+    fs = 8
 
     for j in xrange(0,10):
 
         # pick out all records in a particular class
         mask = tar == np.float64(j)
-        print "    Digit:", j, mask.sum(), len(dat)
 
-        # vectorizing (doing it individually takes very long)
-        weight = np.sum(dat[mask,:], axis = 1)
-        xCM = np.dot(xAll, dat[mask].T) /  weight
-        yCM = np.dot(yAll, dat[mask].T) / weight
-
-        dx = np.zeros( (mask.sum(), 28**2), dtype = np.float64)
-        dy = np.zeros( (mask.sum(), 28**2), dtype = np.float64)
-
-        for i in xrange(0, 28**2):
-            dx[:,i] = xAll[i] - xCM
-            dy[:,i] = yAll[i] - yCM
-
-        # coefficients of moment of inertia tensor
-        a = np.dot( dx**2, dat[mask].T ).diagonal() 
-        b = np.dot( dx * dy, dat[mask].T ).diagonal() 
-        d = np.dot( dy**2, dat[mask].T ).diagonal()
-
-        # trace and determinant
-        Tr = a + d
-        Det = a * d - b * b
-
-        # ratio of eigenvalues
-        vals = ( (Tr / 2.0 + np.sqrt(Tr**2 / 4.0 - Det) ) / 
-                (Tr / 2.0 - np.sqrt(Tr**2 / 4.0 - Det) ) )
-
-        #print rat[:5]
-
-        # now make histogram and plot
-        hist, bin_edges = np.histogram(vals, bins = 30, density=True)
+        # make histogram of ratio of eigenvalues
+        ax = fig.add_subplot(3, 3, 1)
+        hist, bin_edges = np.histogram(datRot[mask,-1], bins = 30, density = True)
         ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
                  hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.axis([0, 40, 0, 0.8])
+        ax.semilogx()
+        ax.set_xlabel(r"Ratio of Rotation Eigenvalues", fontsize = fs)
+        plt.legend(loc='upper right',prop = prop)
 
-    ax.axis([0, 40, 0, 0.8])
-    ax.semilogx()
-    plt.legend(loc='upper right',prop=prop)
-    plt.savefig('Eig.pdf', bbox_inches = 'tight')
+        # zeroth moment
+        ax = fig.add_subplot(3, 3, 2)
+        hist, bin_edges = np.histogram(datRot[mask,0], bins = 30, density = True)
+        ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
+                 hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.set_xlabel(r"0th moment (first radial bin)", fontsize = fs)
+
+        # sin(theta) convolution
+        ax = fig.add_subplot(3, 3, 3)
+        hist, bin_edges = np.histogram(datRot[mask,1], bins = 30, density = True)
+        ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
+                 hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.set_xlabel(r"Sin $\theta$ Convolution (first radial bin)", fontsize = fs)
+
+        # cos(theta) convolution                             
+        ax = fig.add_subplot(3, 3, 4)
+        hist, bin_edges = np.histogram(datRot[mask,2], bins = 30, density = True)
+        ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
+                 hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.set_xlabel(r"Cos $\theta$ Convolution (first radial bin)", fontsize = fs)
+
+        # sin(2 * theta) convolution                             
+        ax = fig.add_subplot(3, 3, 5)
+        hist, bin_edges = np.histogram(datRot[mask,3], bins = 30, density = True)
+        ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
+                 hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.set_xlabel(r"Sin $2\theta$ Convolution (first radial bin)", fontsize = fs)
+
+        # cos(2 * theta) convolution                             
+        ax = fig.add_subplot(3, 3, 6)
+        hist, bin_edges = np.histogram(datRot[mask,4], bins = 30, density = True)
+        ax.plot( 0.5 * (bin_edges[1:] + bin_edges[:-1]),
+                 hist, lw = 2.0, label = str(j), ls = lsv[j])
+        ax.set_xlabel(r"Cos $2\theta$ Convolution (first radial bin)", fontsize = fs)
+
+
+    plt.savefig('RotFeatures.pdf', bbox_inches = 'tight')
     fig.clear()
+
+def TrainSVM(xTrain, xVal, xTest, yTrain, yVal, yTest):
+
+    '''
+    This function finds the optimal parameters for the classifier
+                                                                 
+    Parameters:
+    ----------
+    xTrain: numpy array (training set)    xVal: numpy array (cross validation set)
+    xTest: numpy array (test set)
+    yTrain: numpy array (class labels for training set)
+    yVal: numpy array (class labels for validation set)
+    yTest: numpy array (class lables for test set)
+
+    Returns:
+    --------
+    optim: optimal parameters for the classifier
+    '''
+
+    # choose optimal penalty parameter
+    cVal = [0.1, 0.3, 1.0, 3.0, 10.0, 30.0, 100.0]
+    valScores = np.zeros(len(cVal))
+    testScores = np.zeros(len(cVal))
+
+    for i in xrange(0, len(cVal)):
+        
+        print '     Training with penalty parameter:', cVal[i]
+        clf = svm.SVC(kernel = 'rbf', C = cVal[i])
+        clf.fit(xTrain[:1000,:], yTrain[:1000]) 
+        valScores[i] = clf.score(xVal, yVal)
+        testScores[i] = clf.score(xTest, yTest)
+    
+    print 
+    optim = cVal[np.argmax(valScores)]
+    print 'Optimal parameter is:', optim, '\n'
+    print ('Test set score (on a smaller training set):', 
+           testScores[np.argmax(valScores)])
+    print
+
+    # now plot learning curve to see whether more training data will help
+
+    print 'Generating Learning Curve...'
+
+    szV = range(50, 10000, 1000)
+    LCvals = np.zeros((len(szV), 3),dtype = np.float64) # store data points of learning curve
+    clf = svm.SVC(kernel = 'rbf', C = optim)
+    for i in xrange(0, len(szV)):
+        print '     Training with data set size:', szV[i]
+        clf.fit(xTrain[:szV[i],:], yTrain[:szV[i]])
+        LCvals[i,0] = szV[i]
+        LCvals[i,1] = clf.score(xTest, yTest)
+        LCvals[i,2] = clf.score(xTrain[:szV[i],:], yTrain[:szV[i]])
+
+    print
+    # generate figure (it shows that we're not in the high bias regime)
+    fig = plt.figure(1, figsize = (10, 10))
+    prop = matplotlib.font_manager.FontProperties(size = 15.5)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(LCvals[:,0], 1.0 - LCvals[:,1], label = 'Test Set')
+    ax.plot(LCvals[:,0], 1.0 - LCvals[:,2], label = 'Training Set')
+    ax.set_ylabel(r"Error", fontsize = 20)
+    ax.set_xlabel(r"Training Set Size", fontsize = 20)
+    ax.axis([40.0, 10000, -0.1, 0.5])
+    plt.legend(loc = 'upper right', prop = prop)
+    plt.savefig('LC_Rot_SVM.pdf', bbox_inches = 'tight')
+    fig.clear()
+
+    return optim
+
+def MakePred(xTrain, yTrain, optim, test, xTest, yTest):
+
+    '''
+    This function makes a prediction for the test set
+
+    Parameters:
+    -----------
+    xTrain: numpy array (training set)
+    yTrain: numpy array (class labels of training set)
+    optim: optimal parameter for classification algorithm
+    test: numpy array (test set)
+    xTest: numpy array (set used to gauge performance - subset of initial training set)
+    yTest: numpy array (class labels for xTest)
+
+    '''
+
+    # train model using optimal regularization
+    print '     Training with 15000 samples...'
+    clf = svm.SVC(kernel = 'rbf', C = optim)
+    clf.fit(xTrain[:15000,:], yTrain[:15000]) # takes too long with more data points
+    pred = clf.predict(test)
+    print '     Score on test set:', clf.score(xTest, yTest)
+
+    return pred
+
 
 def main():
 
@@ -88,8 +192,56 @@ def main():
     dat, tar = utils.ReadDat("train.csv", 1)
     print "Done Reading.\n"
 
-    print "Investigating moment of Inertia:"
-    plotHistEig(dat, tar)
+    print 'Calculating Rotation Indpendent Features...'
+    datRot = np.zeros( (len(dat), 16), dtype = np.float64)
+    for j in xrange(0,10):
+
+        print '    Digit', j
+
+        # pick out all records in a particular class 
+        # doing it by class because running on whole
+        # data set takes too long
+        mask = tar == np.float64(j)
+        datRot[mask, :] = utils.CalcRotFeatures(dat[mask])
+
+    print datRot[0,:]
+    print "Plot Distributions of Features...\n"
+    plotHistRot(datRot, tar)
+
+    print "Now training model on features to see performance... \n"
+    # first mean normalization
+    datRot = utils.meanNorm(datRot)
+    # now split up training set into training, cross validation and test set
+    xTrain, xTmp, yTrain, yTmp = cross_validation.train_test_split(datRot, tar, 
+                                                                   test_size = 0.4)
+    xVal, xTest, yVal, yTest = cross_validation.train_test_split(xTmp, yTmp, 
+                                                                 test_size = 0.5)
+    opt = TrainSVM(xTrain, xVal, xTest, yTrain, yVal, yTest)
+
+    # now read in test set
+    print "Reading Test Set..."
+    test, tmp = utils.ReadDat("test.csv", 0)
+    print "Done Reading.\n"
+
+    # Preprocess test set
+    print 'Calculating Rotation Independent Features for Test Set...'
+    dl = len(test)/5
+    testRot = np.zeros( (len(test), 16), dtype = np.float64)
+    for i in xrange(0,5):
+        print '     Chunk', i
+        testRot[i * dl: (i + 1) * dl, :] = utils.CalcRotFeatures(test[i * dl: (i+ 1) * dl, :])
+
+    print testRot[-1,:]
+    testRot = utils.meanNorm(testRot)
+
+    # now make prediction
+    print 'Making Prediction'
+    pred = MakePred(xTrain, yTrain, opt, testRot, xTest, yTest)
+
+    print
+    print 'Outputting prediction\n'
+    utils.OutputPred(pred, ['ImageId','Label'], 2)
+
 
     print 'Done'
 
--- utils.py
+++ utils.py
@@ -79,7 +79,6 @@ def sampleImage(vals):
     Parameters:
     ----------
     vals: numpy array of all the pixel values for a given record
-    
 
     '''
 
@@ -157,10 +156,117 @@ def OutputPred(pred, header, pathV):
 
     '''
 
-    paths = ['predSVM.csv', 'predRF.csv']
+    paths = ['predSVM.csv', 'predRF.csv', 'predRotSVM.csv']
     
     open_file_object = csv.writer(open(paths[pathV], "wb"))
     open_file_object.writerow(header)
 
     for i in xrange(0, len(pred)):
         open_file_object.writerow([i + 1, int(pred[i])])
+
+
+def CalcRotFeatures(dat):
+
+    '''
+    This function calculates orientation independent features. 
+    More specifically it takes into account how the digit is alligned
+    relative to the axis of the image. 
+
+    First we calculate the moment of inertia tensor to find the orientation
+    of the digit. Then we calculate the ratio of the eigenvalues and other moments with
+    respect to angular projections, in different radial bins.
+
+    I Vectorized the calculation.
+
+    Parameters:
+    -----------
+    dat: numpy array of all records
+
+    Returns:
+    --------
+    datRot: various features that are orientation independent
+
+    '''
+
+    # mapping of index in record to x,y coordinates
+    xAll = np.array( [i % 28 for i in xrange(0, 28**2)] )
+    yAll = np.array( [28 - i / 28 for i in xrange(0, 28**2)] )
+
+    # Define mass (weight) and center of mass
+    weight = np.sum(dat, axis = 1)
+    xCM = np.dot(xAll, dat.T) /  weight
+    yCM = np.dot(yAll, dat.T) / weight
+
+    # displacement in x and y of each pixel. radius of each pixel from center
+    dx = np.zeros( (len(dat), 28**2), dtype = np.float64)
+    dy = np.zeros( (len(dat), 28**2), dtype = np.float64)
+    dr = np.zeros( (len(dat), 28**2), dtype = np.float64)
+
+    # new feature array that will be returned
+    # columns are: ratio of eigenvalues of Moment Intertia tensor, and moments of 
+    # pixels wrt angular function (ie: sin(theta), cos(theta), sin(2*theta)...)
+    # also splitting into different radial bins
+    datRot = np.zeros( (len(dat), 16), dtype = np.float64)
+
+    for i in xrange(0, 28**2):
+        dx[:,i] = xAll[i] - xCM
+        dy[:,i] = yAll[i] - yCM
+
+    # useful if you want to split moments into radial bins
+    dr = np.sqrt(dx**2 + dy**2)
+    # define bins
+    bins = np.linspace(0, np.max(dr) * 0.7, num = 4) 
+
+    # coefficients of moment of inertia tensor
+    a = np.sum(dx**2 * dat, axis = 1)
+    b = np.sum(dx * dy * dat, axis = 1)
+    d = np.sum(dy**2 * dat, axis = 1)
+
+    # trace and determinant
+    Tr = a + d
+    Det = a * d - b * b
+
+    # ratio of eigenvalues
+    datRot[:,-1] = ( (Tr / 2.0 + np.sqrt(Tr**2 / 4.0 - Det) ) /
+                    (Tr / 2.0 - np.sqrt(Tr**2 / 4.0 - Det) ) )
+
+    # now moments. first define angle of principal axis
+
+    # angle of rotation relative to vertical for each image
+    eigVecY = Tr / 2.0 + np.sqrt(Tr**2 / 4.0 - Det) - a
+    angle = np.arccos( eigVecY / np.sqrt(eigVecY**2 + b**2) )
+    # takes into account clockwise or counter clockwise
+    cc = np.ones(len(dat), dtype = np.float64) 
+    mask = b < 0
+    cc[mask] = -1.0
+    mask = angle > (np.pi / 2.0)
+    angle[mask] = np.pi - angle[mask]
+    cc[mask] = -1.0 * cc[mask]
+    angle = cc * angle
+
+    # define angle at each pixel
+    theta = np.arctan2(dy, dx)
+    # now shift angles to be relative to principal axis
+    theta = (theta.T + angle).T
+
+    # now take moments for different radial bins
+    for i in xrange(0, len(bins) - 1):
+
+        mask = ( (dr[0,:] <= bins[i+1]) & (dr[0,:] > bins[i]) )
+        datRot[:, i * 5] = ( np.sum(dat[:,mask], axis = 1) 
+                                         / weight )
+        datRot[:, 1 + i * 5] = ( np.sum(dat[:,mask] * np.sin(theta[:,mask]), axis = 1) 
+                                 / weight )
+        datRot[:, 2 + i * 5] = ( np.sum(dat[:,mask] * np.cos(theta[:,mask]), axis = 1) 
+                                 / weight )
+        datRot[:, 3 + i * 5] = ( np.sum(dat[:,mask] * np.sin(2.0 * theta[:,mask]), axis = 1) 
+                                 / weight )
+        datRot[:, 4 + i * 5] = ( np.sum(dat[:,mask] * np.cos(2.0 * theta[:,mask]), axis = 1) 
+                                 / weight )
+ 
+    return datRot
+
+
+
+
+
